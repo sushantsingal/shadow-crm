149aa726fb69cd37502abb9a75875410661a89cb
Sushant Singal <142747451+sushantsingal@users.noreply.github.com>
2025-06-02 14:59:01 +0530

    Update CampaignCard.jsx

--- Mini-CRM-Platform/src/components/CampaignCard.jsx
+++ Mini-CRM-Platform/src/components/CampaignCard.jsx
@@ -1,7 +1,7 @@
 const CampaignCard = ({ name, audienceSize, sent, failed, date }) => {
   return (
-    <div className="bg-white rounded-lg shadow p-4 border border-gray-200">
-      <h3 className="text-lg font-semibold mb-1 text-gray-800">{name}</h3>
+    <div className="bg-white rounded-lg shadow p-4 border border-gray-200 hover:shadow-md transition duration-200">
+      <h3 className="text-lg font-semibold mb-1 text-gray-800 truncate">{name}</h3>
       <p className="text-sm text-gray-600">Audience: {audienceSize}</p>
       <p className="text-sm text-green-600">Sent: {sent}</p>
       <p className="text-sm text-red-500">Failed: {failed}</p>
